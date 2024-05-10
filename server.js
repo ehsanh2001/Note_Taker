d8c0886b5cca51807e0aa4290115b46f04adad55
ehsanh2001 <ehsanh2001@gmail.com>
2024-05-10 15:49:32 -0400

    Create minimum server to serve main page and notes page

---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
